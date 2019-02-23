fcaad3c5133e83dc8265589b5dbcf5941335a158
Marcio Teixeira Quimbundo [DIGITAL FACTORY] <mquimbundo@ucall.co.ao>
2019-02-23 23:35:10 +0100

    class PlayerApiProvider finished

---
+++ lib/src/services/player_api_provider.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:jogadores_football_app/src/models/api_models.dart';
+class PlayerApiProvider {
+  String baseUrl = "https://www.easports.com/fifa/ultimate-team/api/fut/item?";
+
+  fetchPlayersByCountry (String countryId) async{
+    final response = await http.get("$baseUrl+counrtyId=$countryId");
+
+    final responseString = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return ApiResult.fromJson(responseString).items;
+    } else {
+      throw Exception('Failed to load players');
+    }
+  }
+}
